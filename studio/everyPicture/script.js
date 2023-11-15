9d1466d502f8e6bea669c98cf3413904282d83a6
Emily Kang <eykang@ucdavis.edu>
2023-11-15 02:18:53 -0800

    message

--- studio/everyPicture/script.js
+++ studio/everyPicture/script.js
@@ -1,26 +1,26 @@
 (function () {
     'use strict';
+    
 
-    window.addEventListener('scroll', function () {
-        var fixedBlock = document.getElementById('fixedBlock');
-        var secondSection = document.getElementById('second');
-        var offset = secondSection.offsetTop - window.scrollY;
-
-        if (offset < 0) {
-            fixedBlock.classList.add('show');
-        } else {
-            fixedBlock.classList.remove('show');
-        }
+    document.addEventListener("DOMContentLoaded", function() {
+        var columns = document.querySelectorAll('.column');
+    
+        columns.forEach(function(column) {   // Add a mouseover event listener to each column
+            column.addEventListener('mouseover', function() {
+                // Get the image element inside the column
+                var image = column.querySelector('img');
+    
+                // Slide the image to the right
+                image.style.transform = 'translateX(800px)'; // As you increase the px it increases the amount the column slides to the right.
+            });
+    
+            // Add a mouseout event listener to help reset the image position. It will go back to the left.
+            column.addEventListener('mouseout', function() {
+                var image = column.querySelector('img');// helps get the image element inside of the column.
+    
+                image.style.transform = 'translateX(0)'; // Resets the image position
+            });
+        });
     });
-   
-    
-    
-    
-    
-    
-    
-
-
-
 
 })();
